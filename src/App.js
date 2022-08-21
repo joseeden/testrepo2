ac33ca6d1e4b343d87e0bd536bc30f3cc61b0211
joseeden <josemanuelitoeden@gmail.com>
2022-08-21 10:52:12 +0000

    Edited file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  To infinity and beyond!
+	  Release the Kraken!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Devops now
+          Learn React
         </a>
       </header>
     </div>
